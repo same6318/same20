cd2a8e2b9bec51b81d62db8f894ef4cd8c81bf72
same06 <typewreter@yahoo.co.jp>
2024-06-18 12:00:56 +0900

    課題

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,26 @@
 class FavoritesController < ApplicationController
 
   def create
-    current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    #current_user.favorites.create(blog_id: params[:blog_id])
+    @favorite = current_user.favorites.build(blog_id: params[:blog_id])
+    @favorite.save
+    respond_to do |format|
+      format.js { render :index }
+      #format.html { redirect_to blogs_path }
+    end
+    #redirect_to blogs_path
   end
 
   def destroy
-    current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    #current_user.favorites.find_by(id: params[:id]).destroy
+    @favorite = current_user.favorites.find_by(id: params[:id])
+    @favorite.destroy
+      respond_to do |format|
+        format.js { render :index }
+        #format.html { redirect_to blogs_path }
+      end
+      #redirect_to blogs_path
   end
+
 
 end
